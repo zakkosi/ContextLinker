31cdd8cfae69f2b3fae5fb1c3004b90380bba34a
JONGHWAN <bjh0309@gmail.com>
2025-09-01 17:59:04 +0900

    FULL_PIPELINE(except schrodinger module)

--- processors/__init__.py
+++ processors/__init__.py
@@ -1,6 +1,6 @@
 from .sam2_processor import SAM2Segmenter
 from .colpali_processor import ColPaliProcessor
 from .llm_processor import LLMProcessor
-from .scrodinger_processor import SchrodingerBridgeProcessor
+from .schrodinger_processor import SchrodingerBridgeProcessor
 
 __all__ = ['SAM2Segmenter', 'ColPaliProcessor', 'LLMProcessor', 'SchrodingerBridgeProcessor']
